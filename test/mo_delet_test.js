2e58104ce50f845a2cd5eb76f3aad7cf6eb172e4
G Vamshi Krishna <vamshiginna1606@gmail.com>
2020-05-08 02:38:12 +0530

    Added mo_delete_test.js file

--- test/mo_delet_test.js
+++ test/mo_delet_test.js
@@ -4,6 +4,21 @@ const assert = require('assert');
 describe("Delete Test", ()=>{
 let deleter;
 
+beforeEach(() => {
+    deleter = new Student({name:"Deleter"});
+    deleter.save()
+    .then(() => done());
+});
+
+it("A delete test for deleter", () => {
+    Student.findByIdAndDelete(deleter._id)
+    .then(() => Student.findOne({name: "Deleter"}))
+    .then((student) => {
+        assert(student === null);
+        done();
+        
+    });
+})
 
 
 
