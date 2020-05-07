91aeb5f2873cce9f97ebc1327e414742eae6538c
G Vamshi Krishna <vamshiginna1606@gmail.com>
2020-05-08 01:57:06 +0530

    Added a mocha test file for creation of records

---
+++ test/mo_create_test.js
@@ -0,0 +1,9 @@
+const Student = require('../app/student');
+const assert = require('assert');
+
+describe('Create records', () => {
+    it('create a user in DB', () =>{
+        //assert(true);
+
+    })
+}  )
