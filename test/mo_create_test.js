9058ad3a6dbbc6e7b61c633a154a789d0ea490bd
G Vamshi Krishna <vamshiginna1606@gmail.com>
2020-05-08 02:07:08 +0530

    Running the mocha test file

--- test/mo_create_test.js
+++ test/mo_create_test.js
@@ -4,6 +4,15 @@ const assert = require('assert');
 describe('Create records', () => {
     it('create a user in DB', () =>{
         //assert(true);
+        const naga = new Student({name: "naga"});
+        naga.save()
+        .then( ()=>{
+            assert(!naga.isNew)
+        }
 
+        )
+        .catch(()=>{
+            console.log("error!")
+        })
     })
 }  )
